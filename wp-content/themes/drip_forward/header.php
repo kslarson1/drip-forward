f159fc2eac9a5f8b947bc3f22d9b0365b23518f6
Karl Larson <KarlLarson@Karls-MacBook-Air.local>
2014-10-25 17:02:59 -0600

    Contact Form 7 Added
    finally added the damn contact form, but no style added yet

--- wp-content/themes/drip_forward/header.php
+++ wp-content/themes/drip_forward/header.php
@@ -9,6 +9,7 @@
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
+<link href='http://fonts.googleapis.com/css?family=Lato:100,300,400,700,300italic' rel='stylesheet' type='text/css'>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title><?php wp_title( '|', true, 'right' ); ?></title>
--- wp-content/themes/drip_forward/templates/home_page.php
+++ wp-content/themes/drip_forward/templates/home_page.php
@@ -15,14 +15,13 @@ get_header(); ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<?php get_template_part( 'content', 'page' ); ?>
-				
 				<!-- START OF HERO SECTION -->
-				<div id="home_section" class="full-width" style="background-image: url(<?php the_field(home_hero_image); ?>)">
-				</div>
+				<div id="home_section" class="full-width" style="background-image: url(<?php the_field('home_hero_image'); ?>);" >
 					<!-- START OF TITLE AND TEXT -->
 				<div class="row">
 					<h1><?php the_field('home_hero_title'); ?></h1>
 					<h3><?php the_field('home_hero_text'); ?></h3>
+				</div>
 				</div>
 					<!-- END OF TITLE AND TEXT -->
 				<!-- END OF HERO SECTION -->
@@ -59,22 +58,29 @@ get_header(); ?>
 				<!-- START OF WHY US SECTION -->
 				<div id="why_section">
 					<!-- WHY US TITLE -->
-				<div class="row">
-				<h1><?php the_field('why_title'); ?></h1>
-				</div>
+					<div class="row">
+						<h1><?php the_field('why_title'); ?></h1>
+					</div>
 					<!-- END OF WHY US TITLE -->
 					<!-- WHY US IMAGE -->
-					<div class="rounded_image" img src="<?php the_field('why_image'); ?>">
-					</div>
+					<div class="row">
+						<div class="col-1-12">
+						<img src="<?php the_field('why_image'); ?>">
+						</div>
 					<!-- WHY US TEXT -->
 					<p><?php the_field('why_text'); ?></p>
 					<!-- START OF TESTIMONIAL SECTION -->
 					<!-- END OF TESTIMONIAL SECTION -->
 				</div>
+				</div>
 				<!-- END OF WHY US SECTION -->
 
 				<!-- START OF CONTACT SECTION -->
 				<div id="contact_section">
+				<div class="row">
+					<p>Your Name (required)</p>
+    <?php echo do_shortcode('[contact-form-7 id="38" title="Contact form"]'); ?>
+				</div>
 				</div>
 				<!-- END OF CONTACT SECTION -->
 
